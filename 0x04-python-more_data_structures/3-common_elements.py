bdb9b5b582594fe9c7b5180d24f4cfc4f5aa2279
charlesnjagi254 <charlesnjagi51@gmail.com>
2022-09-29 05:12:44 -0700

    Task 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,4 +1,9 @@
 #!/bin/usr/python3
 
 def common_elements(set_1 & set_2):
-    return set_1 & set_2
+    result = False
+    for x in set_1:
+        for y in set_2:
+            if x ==y:
+                result = True
+                return result
