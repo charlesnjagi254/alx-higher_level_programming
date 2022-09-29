51f7f6ae419c09135462914b99cf2bcf0d477f40
charlesnjagi254 <charlesnjagi51@gmail.com>
2022-09-29 03:15:05 -0700

    Task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,5 +2,5 @@
 def search_repace(my_list, search, replace):
     #my_list = [1,2,3,4,5,4,3,2,89]
     #new_list = search_replace(my_list,56,23)#
-    new_list = list(map(lambda x: replace if == search else x, my_list))
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
     return (new_list)
