0c8fff0d2a2c2fe4b0f11494b38039af88606d74
charlesnjagi254 <charlesnjagi51@gmail.com>
2022-12-19 21:12:55 -0800

    Task 2 prints arguments if found

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (process.argv.length === 2) {
+	console.log('No argument');
+} else if (process.argv.length === 3) {
+	consolle.log('Argument foound');
+} else {
+	console.log('Arguments found');
+}
